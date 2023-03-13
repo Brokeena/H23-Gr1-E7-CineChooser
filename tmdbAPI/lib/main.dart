85e2eebc5f8600a04f2bd5f1374972e7e4d55802
Brokeena <ch.obser@outlook.com>
2023-03-13 17:38:30 -0400

    Re organize movie class

---
+++ tmdbAPI/lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:collection';
+import 'package:tmdb_api/tmdb_api.dart';
+import 'movie.dart';
+import 'api.dart';
+
+
+
+
+
+Future<void> main(List<String> arguments) async
+{
+  Movie movie = new Movie(545611);
+  // https://github.com/dart-lang/sdk/issues/23115
+
+  print(movie);
+
+
+}
+
+
+
+
+
+
+
+
--- tmdbAPI/lib/movie.dart
+++ tmdbAPI/lib/movie.dart
@@ -1,46 +1,34 @@
-import 'dart:collection';
+import 'api.dart';
 
-import 'package:tmdb_api/tmdb_api.dart';
+ class  Movie{
+  String? nom;
+  int? dateDeSortie;
+  String? poster;
+  String? overview;
+  List<String>? directors;
+  List<String>? actors;
+  List<String>? providers;
+  double? score;
+  int id;
+  int? runtime;
+  List<int> genresId = [];
+  List<Genre> genres = [];
 
-String country = 'CA';
 
-final tmdb = TMDB( //TMDB instance
-  ApiKeys('e99858b3004ef91a1e786cc7f99fde94', 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTk4NThiMzAwNGVmOTFhMWU3ODZjYzdmOTlmZGU5NCIsInN1YiI6IjYzZWE5OTRmOGU4NzAyMDBkYzdiN2FlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qOpj0nwkYQhC-8nubR4b-gT2qyJLPA65kCvWVl69ZkE'),
-  logConfig: ConfigLogger(
-    showLogs: true,//must be true than only all other logs will be shown
-    showErrorLogs: true,
-  ), defaultLanguage:'en-' + country,
-);
-
-
-Future<void> main(List<String> arguments) async {
-
-  //Method
-  List<String> findJobIndexInList(List<dynamic> searchIn, String searchTerm){
-    List<String> jobs = ["unknown"];
-    for(int i = 0; i < searchIn.length ; i++){
-      if(searchIn.elementAt(i)['job'] == searchTerm){
-        if(jobs.elementAt(0) == "unknown"){
-          jobs.clear();
-        }
-        jobs.add(searchIn.elementAt(i)['name']);
-      }
-    }
-    return jobs;
+  Movie(this.id) {
+    setDetails(id);
+    attribuerGenre();
+    setDirector(id);
+    setActors(id);
+    setWatchProviders(id);
   }
 
 
-  List<String> findFirstActors(List<dynamic> searchIn, int nbOfActor){
-    List<String> actors = ["unknown"];
-    for(int i = 0; i < nbOfActor; i++){
-      if(actors.elementAt(0) == "unknown"){
-        actors.clear();
-      }
-      actors.add(searchIn.elementAt(i)['name']);
-    }
-    return actors;
-  }
-
+  /// Méthode pour créer correctement un film
+  /// *********************************
+  /// *********************************
+  /// *********************************
+  /// *********************************
 
   int formatDate(String date){
     String dateF = "";
@@ -50,72 +38,57 @@ Future<void> main(List<String> arguments) async {
     return int.parse(dateF);
   }
 
-
-
-  
-  //Research
-  Map search = await tmdb.v3.search.queryMovies("Zombieland");
-  List<dynamic> result = search['results'];
-  Map movie = await tmdb.v3.movies.getDetails(result.elementAt(0)['id']);
-  
-  //Find directors
-  Map creditsDetails = await tmdb.v3.movies.getCredits(result.elementAt(0)['id']);
-  List crew = creditsDetails['crew'];
-
-  //Find actors
-  List cast = creditsDetails['cast'];
-  List<String> actors = findFirstActors(cast, 3);
-
-  //Genres
-  List<dynamic> genresList = movie['genres'];
-  List<int>? genresId = [];
-  for(int i = 0; i < genresList.length; i++){
-    genresId.add(genresList.elementAt(i)['id']);
+  void setDetails(int id) async {
+    Map movieRaw = await tmdb.v3.movies.getDetails(id);
+    nom = movieRaw['title'];
+    dateDeSortie = formatDate(movieRaw['release_date']);
+    runtime = movieRaw['runtime'];
+    score = movieRaw['vote_average'];
+    poster = "https://image.tmdb.org/t/p/original"  + movieRaw['poster_path'];
   }
 
-  //Providers
-  Map searchWatchProviders = await tmdb.v3.movies.getWatchProviders(result.elementAt(0)['id']);
-  List providers = searchWatchProviders['results'][country]['flatrate'];
-  List<String> providersName = [];
-  for( var value in providers){
-    providersName.add(value['provider_name']);
+  void setDirector(int id) async {
+    Map creditsDetails = await tmdb.v3.movies.getCredits(id);
+    List crew = creditsDetails['crew'];
+    List<String> directorsFound = ["unknown"];
+    for(int i = 0; i < crew.length ; i++){
+      if(crew.elementAt(i)['job'] == "Director"){
+        if(directorsFound.elementAt(0) == "unknown"){
+          directorsFound.clear();
+        }
+        directorsFound.add(crew.elementAt(i)['name']);
+      }
+    }
+
+    directors = directorsFound;
+
   }
 
+  void setActors(int id) async {
+    Map creditsDetails = await tmdb.v3.movies.getCredits(id);
+    List cast = creditsDetails['cast'];
+    List<String> actorsFound = ["unknown"];
+    for(int i = 0; i < 3; i++){
+      if(actorsFound.elementAt(0) == "unknown"){
+        actorsFound.clear();
+      }
+      actorsFound.add(cast.elementAt(i)['name']);
+    }
 
-
-
-  Movie movieSearched = new Movie(movie['title'], formatDate(movie['release_date']), movie['overview'], findJobIndexInList(crew, "Director"), actors, movie['runtime'], movie['vote_average'], providersName, "https://image.tmdb.org/t/p/original"  + movie['poster_path'], movie['id'],genresId);
-
-
-  
-  print(providersName);
-  print("-----------------------");
-  print(movieSearched.toString());
-
-
-
-}
-
-
-class Movie{
-  String nom;
-  int dateDeSortie;
-  String poster;
-  String overview;
-  List<String> director;
-  List<String> actors;
-  List<String> providers;
-  double score;
-  int id;
-  int runtime;
-  List<int> genresId = [];
-  List<Genre> genres = [];
-
-
-  Movie(this.nom,this.dateDeSortie, this.overview, this.director, this.actors, this.runtime, this.score, this.providers, this.poster, this.id, this.genresId){
-    attribuerGenre();
+    actors = actorsFound;
   }
-  
+
+  void setWatchProviders(int id) async {
+    Map searchWatchProviders = await tmdb.v3.movies.getWatchProviders(id);
+    List providersFound = searchWatchProviders['results'][country]['flatrate'];
+    List<String> providersNames = [];
+    for( var value in providersFound){
+      providersNames.add(value['provider_name']);
+    }
+    providers = providersNames;
+
+  }
+
 
   void attribuerGenre(){
     for(var value in Genre.values ){
@@ -127,13 +100,20 @@ class Movie{
     }
   }
 
+  Future<bool> check() async{
+    if(nom!=null){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
 
   @override
-  String toString() => 'Film trouvée: \n nom: $nom, \n dateDeSortie: $dateDeSortie, \n director: $director, \n actors: $actors, \n runtime: $runtime, \n synopsis: $overview, \n score/10: $score, \n genres: $genres, \n watch on: $providers, \n poster: $poster \n id TMDB: $id ';
+  String toString() => 'Film trouvée: \n nom: $nom, \n dateDeSortie: $dateDeSortie, \n director: $directors, \n actors: $actors, \n runtime: $runtime, \n synopsis: $overview, \n score/10: $score, \n genres: $genres, \n watch on: $providers, \n poster: $poster \n id TMDB: $id ';
 
 
 }
-
 
 enum Genre {
   Action(id: 28, name: 'action'),
@@ -157,16 +137,27 @@ enum Genre {
   Western(id: 37, name: 'western');
 
 
- 
+
 
 
   const Genre({
-   required this.id,
-   required this.name,
-});
+    required this.id,
+    required this.name,
+  });
 
   final int id;
   final String name;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
