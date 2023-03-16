6061f353c99e1b5331afb6078d98fcc2d4d03940
Brokeena <ch.obser@outlook.com>
2023-03-16 12:55:33 -0400

    Fixing async constructor
    J'ai trouvé un moyen de fix le probleme du constructeur async, fuat juste que je l'écrive

--- tmdbAPI/lib/main.dart
+++ tmdbAPI/lib/main.dart
@@ -10,6 +10,7 @@ import 'api.dart';
 Future<void> main(List<String> arguments) async
 {
   Movie movie = new Movie(545611);
+  // https://stackoverflow.com/questions/38933801/calling-an-async-method-from-a-constructor-in-dart
   // https://github.com/dart-lang/sdk/issues/23115
 
   print(movie);
