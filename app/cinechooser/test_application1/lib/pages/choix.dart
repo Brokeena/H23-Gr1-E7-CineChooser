178904758f28a69bcb1d8c0aed32ca0e2a0c151d
Marriusse <123408939+Marriusse@users.noreply.github.com>
2023-05-01 17:02:18 -0400

    texte movie case

--- app/cinechooser/test_application1/lib/pages/choix.dart
+++ app/cinechooser/test_application1/lib/pages/choix.dart
@@ -20,11 +20,11 @@ class Choix extends StatefulWidget {
 
 class _ChoixState extends State<Choix> {
   List<int> listGenre = [];
+  double tailleBordure = 0;
+
 
   @override
   Widget build(BuildContext context) {
-    print(listGenre);
-    print("choiz page");
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Styles.bgColor,
--- app/cinechooser/test_application1/lib/widget/MovieCase.dart
+++ app/cinechooser/test_application1/lib/widget/MovieCase.dart
@@ -1,7 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cinechooser/widget/button_carre.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cinechooser/pages/choix.dart';
 import '../utils/app_styles.dart';
 
 class MovieCase extends StatelessWidget {
@@ -16,31 +16,57 @@ class MovieCase extends StatelessWidget {
   final String nom;
   final String image;
 
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-
 
     return ClipRRect(
       child: Stack(children: [
         SizedBox(
           height: width / 4,
           width: width / 4,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                  image: NetworkImage(image), fit: BoxFit.cover),
+          child: Stack(children: [
+            Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                        image: NetworkImage(image), fit: BoxFit.cover),
+                    border: Border.all(
+                        color: Colors.transparent,
+                        width: 1.5)),
+              ),
             ),
-          ),
+            Positioned(
+              bottom: 0,
+              //top: width/6,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: EdgeInsets.all(7),
+                child: AutoSizeText(
+                  nom,
+                  //maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
+        /*
         Center(
-            child: Padding(
-              padding: EdgeInsets.only(top: width / 5),
-              child: AutoSizeText(nom, style: TextStyle(
-                color: Styles.white1
-              )),
-        )),
+          child: AutoSizeText(nom, style: TextStyle(
+            color: Styles.white1
+          )),
+        ),
+
+         */
         Button(
             onPressed: onPressed,
             icone: const Icon(
