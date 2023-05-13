7161f034506749f72e9819ac39ad9811461cefea
Brokeena <ch.obser@outlook.com>
2023-05-13 18:05:46 -0400

    Loading page when register

--- app/cinechooser/test_application1/lib/pages/loading_page.dart
+++ app/cinechooser/test_application1/lib/pages/loading_page.dart
@@ -1,19 +1,20 @@
 import 'package:cinechooser/pages/pagePrincipale.dart';
+import 'package:cinechooser/pages/reglages_first_time.dart';
 import 'package:cinechooser/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'login_page.dart';
 
-class LoadingPage extends StatefulWidget {
-  const LoadingPage({Key? key}) : super(key: key);
+class LoadingPageLogin extends StatefulWidget {
+  const LoadingPageLogin({Key? key}) : super(key: key);
 
   @override
-  State<LoadingPage> createState() => _LoadingPageState();
+  State<LoadingPageLogin> createState() => _LoadingPageLoginState();
 }
 
 
-class _LoadingPageState extends State<LoadingPage> {
+class _LoadingPageLoginState extends State<LoadingPageLogin> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -40,4 +41,39 @@ class _LoadingPageState extends State<LoadingPage> {
 
   }
 }
+
+class LoadingPageRegister extends StatefulWidget {
+  const LoadingPageRegister({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingPageRegister> createState() => _LoadingPageRegisterState();
+}
+
+class _LoadingPageRegisterState extends State<LoadingPageRegister> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double heigth = MediaQuery.of(context).size.height;
+    return ValueListenableBuilder<bool>(
+      valueListenable: hasData,
+      builder: (context, value, child) {
+        if (value == true) {
+          return const ReglagesFirstTime();
+        } else {
+          return Scaffold(
+            backgroundColor: Styles.bgColor,
+            body: Center(
+              child: SpinKitThreeBounce(
+                size: width/10,
+                color: Styles.red1,
+                duration: const Duration(seconds: 1),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
 
