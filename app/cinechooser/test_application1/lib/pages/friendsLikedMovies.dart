b54205130087982df6fb826e12c86e11b78c4171
Brokeena <ch.obser@outlook.com>
2023-05-14 20:31:23 -0400

    create friendsLikedMovies.dart

---
+++ app/cinechooser/test_application1/lib/pages/friendsLikedMovies.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:cinechooser/utils/app_styles.dart';
+import 'package:cinechooser/pages/login_page.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class FriendLikedMovies extends StatefulWidget {
+  const FriendLikedMovies({Key? key}) : super(key: key);
+
+  @override
+  State<FriendLikedMovies> createState() => _FriendLikedMoviesState();
+}
+
+class _FriendLikedMoviesState extends State<FriendLikedMovies> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
